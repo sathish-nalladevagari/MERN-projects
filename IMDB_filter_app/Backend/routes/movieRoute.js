a34d2555c4aaae286a094520fba977ec2dd1ba92
Sathish reddy <104757035+sathish-nalladevagari@users.noreply.github.com>
2024-03-23 22:33:26 +0530

    implemented full search, filter, sort, pagination

--- IMDB_filter_app/Backend/routes/movieRoute.js
+++ IMDB_filter_app/Backend/routes/movieRoute.js
@@ -1,41 +1,42 @@
 import express from "express"
 import { Movies } from "../models/movieModel.js"
+import { parse } from "dotenv"
 
 const router = express.Router()
 
 
 router.get("/movies", async (req,res)=>{
     console.log(req.query)
-
-
-    //parse filters 
-    const page = parseInt(req.query.page) -1 || 0;
-    const limit = parseInt(req.query.limit) || 5;
-    const search = req.query.search || "";
-    let sort = req.query.sort || "rating";
-    let genre = req.query.genre || "All"
     
+   try {
+    const page = parseInt(req.query.page)-1 || 0;
+    const limit = parseInt(req.query.limit) || 5;
+    const search = req.query.search || ""
+    let genre = req.query.genre || "All";
+    let sort = req.query.sort || "rating"
     const genreOptions = [
-        "Action",
-        "Romance",
-        "Fantasy",
-        "Drama",
-        "Crime",
-        "Adventure",
-        "Thriller",
-        "Sci-fi",
-        "Music",
-        "Family",
-    ];
+            "Action",
+            "Romance",
+            "Fantasy",
+            "Drama",
+            "Crime",
+            "Adventure",
+            "Thriller",
+            "Sci-fi",
+            "Music",
+            "Family",
+        ];
     genre === "All" ? (genre = [...genreOptions]) : (genre = req.query.genre.split(","))
-    req.query.sort ? (sort = req.query.sort.split(",")): (sort = [sort])
+    req.query.sort ? ( sort = req.query.sort.split(",")) : (sort = [sort])
 
     let sortBy = {}
-    if(sort[1]){
+    console.log(sort)
+    if (sort[1]){
         sortBy[sort[0]] = sort[1]
     }else{
         sortBy[sort[0]] = "asc"
     }
+
 
     const movies = await Movies.find({name:{$regex : search, $options:"i"}})
     .where("genre")
@@ -43,14 +44,13 @@ router.get("/movies", async (req,res)=>{
     .sort(sortBy)
     .skip(page * limit)
     .limit(limit)
-
+    console.log(movies)
     const total = await Movies.countDocuments( 
-        {
-            genre : {$in:[...genre]},
-            name:{$regex:search,$options:"i"}
-        }
-    )
-
+            {
+                genre : {$in:[...genre]},
+                name:{$regex:search,$options:"i"}
+            }
+        )
     const response = {
         error : false,
         total,
@@ -59,32 +59,18 @@ router.get("/movies", async (req,res)=>{
         genres:genreOptions,
         movies
     }
-    res.status(200).json(response)
-    // try {
-    //     const queryBuilder = await Movies.find(filters)
-    //     console.log(queryBuilder)
+    return res.status(200).json(response)
 
-    //     //sorting
-    //     if(sortBy && sortOrder){
-    //         const sortOptions = {}
-    //         sortOptions[sortBy] = sortOrder === "asc" ? 1 : -1;
-    //         queryBuilder = queryBuilder.sort(sortOptions)
-    //     }
-    //     //pagination
-    //     const movies = await queryBuilder.skip(skip).limit(limitValue).exec()
-    //      // Count total number of documents for pagination
-    //      const totalCount = await Book.countDocuments(filters);
-    //      res.json({
-    //         status: 'success',
-    //         data: books,
-    //         totalItems: totalCount
-    //     });
-    // } catch (error) {
-    //     res.status(500).json({
-    //         status: 'error',
-    //         message: 'Internal server error'
-    //     });
-    // }
+
+   } catch (error) {
+    return res.json({
+        error:true,
+        message:error.message
+    })
+   }
+
+    
+    
 })
 
 
