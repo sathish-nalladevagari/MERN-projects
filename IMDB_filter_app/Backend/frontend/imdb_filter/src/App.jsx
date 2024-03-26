667912971c0c1a1693d31a29762d0e4d130e8396
Sathish reddy <104757035+sathish-nalladevagari@users.noreply.github.com>
2024-03-26 14:08:06 +0530

    project completed

--- IMDB_filter_app/Backend/frontend/imdb_filter/src/App.jsx
+++ IMDB_filter_app/Backend/frontend/imdb_filter/src/App.jsx
@@ -1,34 +1,65 @@
 import { useEffect, useState } from "react"
 import axios from "axios";
 import { BACKEND_URL } from "./config";
+import Search from "./Components/Search/Search";
+import Table from "./Components/Table/Table";
+import Pagination from "./Components/Pagination/Pagination";
+import Sort from "./Components/Sort/Sort";
+import Genre from "./Components/Genre/Genre";
 
 const base_url = BACKEND_URL
 function App() {
-  const [obj, setObj] = useState({});
-  const [sort , setSort] = useState({sort : "rating", order : "desc"})
-  const [filterGenre, setFilterGenre] = useState([])
-  const [page, setPage] = useState(1)
-  const [search, setSearch] = useState("")
-    useEffect(()=>{
-      const getAllMovies = async()=>{
-        try {
-          const url = `${base_url}?page=${page}&sort=${sort.sort}&genre=${filterGenre.toString()}&search=${search}`
-          const {data} = await axios.get(url)
-          setObj(data)
-          console.log(data.movies)
-        } catch (error) {
-          console.log(error.message)
-        }
-      }
-      getAllMovies()
-    },[page,filterGenre,search,sort])
+	const [obj, setObj] = useState({});
+	const [sort, setSort] = useState({ sort: "rating", order: "desc" });
+	const [filterGenre, setFilterGenre] = useState([]);
+	const [page, setPage] = useState(1);
+	const [search, setSearch] = useState("");
 
-    return (
-      <>
-        <h1>Hello world</h1>
-      </>
-    )
-  
+	useEffect(() => {
+		const getAllMovies = async () => {
+			try {
+				const url = `${base_url}?page=${page}&sort=${sort.sort},${
+					sort.order
+				}&genre=${filterGenre.toString()}&search=${search}`;
+				const { data } = await axios.get(url);
+				setObj(data);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+
+		getAllMovies();
+	}, [sort, filterGenre, page, search]);
+
+	return (
+		<div className="wrapper">
+			<div className="container">
+				<div className="head">
+					<img src="./images/logo.png" alt="logo" className="logo" />
+					<Search setSearch={(search) => setSearch(search)} />
+				</div>
+				<div className="body">
+					<div className="table_container">
+						<Table movies={obj.movies ? obj.movies : []} />
+						<Pagination
+							page={page}
+							limit={obj.limit ? obj.limit : 0}
+							total={obj.total ? obj.total : 0}
+							setPage={(page) => setPage(page)}
+						/>
+					</div>
+					<div className="filter_container">
+						<Sort sort={sort} setSort={(sort) => setSort(sort)} />
+						<Genre
+							filterGenre={filterGenre}
+							genres={obj.genres ? obj.genres : []}
+							setFilterGenre={(genre) => setFilterGenre(genre)}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
